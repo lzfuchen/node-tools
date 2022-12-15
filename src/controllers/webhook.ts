00bdea33e82cd78f50f98050470d01842ee93913
fuchen <284992984@qq.com>
2022-12-15 16:34:16 +0800

    feat: 增加自动化部署代码

--- src/controllers/webhook.ts
+++ src/controllers/webhook.ts
@@ -5,17 +5,29 @@ import { exec } from 'child_process'
 export async function push(ctx: Context, next: () => Promise<any>) {
   const { repository: { full_name = '' } = {}, head_commit: { message = '' } = {} } = (ctx.request.body ?? {}) as any
 
-  if (message === 'deploy' && full_name === 'lzfuchen/blog') {
-    // 执行脚本
-    exec('sh /usr/local/src/myblog/docker-start-myblog.sh', (err, stdout, stderr) => {
-      if (err) {
-        console.error(err)
-        return
-      }
-      console.log(`stdout: ${stdout}`)
-      console.log(`stderr: ${stderr}`)
-    })
+  if (message === 'deploy') {
+    if (full_name === 'lzfuchen/blog') {
+      // 执行脚本
+      exec('sh /usr/local/src/myblog/docker-start-myblog.sh', (err, stdout, stderr) => {
+        if (err) {
+          console.error(err)
+          return
+        }
+        console.log(`stdout: ${stdout}`)
+        console.log(`stderr: ${stderr}`)
+      })
+    } else if (full_name === 'lzfuchen/tools-server') {
+      exec('sh ../../deploy.sh', (err, stdout, stderr) => {
+        if (err) {
+          console.error(err)
+          return
+        }
+        console.log(`stdout: ${stdout}`)
+        console.log(`stderr: ${stderr}`)
+      })
+    }
   }
+  ctx.$success()
 }
 
 export async function test(ctx: Context, next: () => Promise<any>) {
